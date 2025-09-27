9243adb984c1732cfaca03cf0d6885f21b310f32
Shashwat <awesomedude019@gmail.com>
2025-09-28 05:12:14 +0530

    header is perfect now

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,25 @@ import Image from "next/image"
 
 export default function Header(){
     return(
-        <div className="flex flex-row items-center bg-teal-400 z-999 sticky top-0">
-            <Image 
-            width={25}
-            height={100}
-            src="/logo.png"
-            alt="my logo"
-            ></Image>
-            <div className="w-350"></div>
-            <div className="w-75">Hi!</div>
-            <div className="w-75">Projects</div>
-            <div className="w-25">About</div>
+        <div className="flex flex-row bg-teal-400 z-999 sticky top-0 gap-140">
+            {/* container for image*/}
+            <div className="basis-64 ">
+                    <Image 
+                    width={25}
+                    height={100}
+                    src="/logo.png"
+                    alt="my logo"
+                    />
+            </div>
+
+            {/*container for other icons */}
+            <div className="basis-128 grid grid-cols-3">
+                <div className=" p-3 w-auto text-center ">hi</div>
+                <div className=" p-3 w-auto text-center ">Projects</div>
+                <div className=" p-3 w-auto text-center ">about</div>
+
+            </div>
+
         </div>
     )
 }
