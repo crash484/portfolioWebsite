b7e2fb7d7b578dd35aaf3b512a018f0bd6d74dcb
Shashwat <awesomedude019@gmail.com>
2025-09-28 06:00:53 +0530

    done

---
+++ src/components/profile.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function Profile(){
+    return(
+        <div className="bg-teal-300 mx-0.5 h-100 rounded-2xl flex items-center justify-center">
+            <div className="w-48 h-48 rounded-full p-10 bg-blue-500"> me</div>
+
+
+        </div>
+    )
+}
