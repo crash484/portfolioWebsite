3004f71132ffe4dade343aa7d5443eabb50dd7a3
Shashwat <awesomedude019@gmail.com>
2025-10-07 03:22:13 +0530

    carousel working ahaha

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,12 +36,27 @@ export default function About(){
 
                <button onClick={prevSlide}
                         className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-black opacity-50 text-white p-2 rounded-full hover:opacity-70">
-                            back
+                            ←
                         </button>
                <button onClick={nextSlide}
                         className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black opacity-50 text-white p-2 rounded-full hover:opacity-70">
-                            next
+                            →
                         </button>
+                         {/* Dots Indicator */}
+                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+                    {images.map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => setCurrentIndex(index)}
+                            className={`w-3 h-3 rounded-full ${
+                                index === currentIndex 
+                                    ? 'bg-white' 
+                                    : 'bg-white opacity-50'
+                            }`}
+                        />
+                    ))}
+                </div>
+
             </div>
 
             {/*text about me*/}
